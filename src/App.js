1e9df053602634340142656ac1e4d7804eeb3633
ishola10 <akoredeishola10@gmail.com>
2024-03-19 02:29:15 -0600

    ght

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React from "react";
 // import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Topnav from './commponents/Navbar';
-import { BrowserRouter as Routes, Route } from "react-router-dom";import Home from './commponents/Pages/Home';
-import Community from './commponents/Pages/Community';
-import Aboutus from './commponents/Pages/Aboutus';
-import Explore from './commponents/Pages/Explore';
-import Faq from './commponents/Pages/Faq';
-import Product from './commponents/Pages/Product';
-import Error from './commponents/Pages/Error';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Topnav from "./commponents/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Community from "./Pages/Community";
+import Aboutus from "./Pages/Aboutus";
+import Explore from "./Pages/Explore";
+import Faq from "./Pages/Faq";
+import Product from "./Pages/Product";
+import Error from "./Pages/Error";
 
 function App() {
   return (
     <>
-    <Topnav />
-    <Routes>
-      <Route Path="/" element={ <Home /> } />
-      <Route Path="/community" element={ <Community /> } />
-      <Route Path="/aboutus" element={ <Aboutus /> } />
-      <Route Path="/explorer" element={ <Explore /> } />
-      <Route Path="/faq" element={ <Faq /> } />
-      <Route Path="/product" element={ <Product /> } />
-      <Route Path="*" element={ <Error /> } />
-    </Routes>
+      <Topnav />
+      <Router>
+        <Routes>
+          <Route Path="/" element={<Home />} />
+          <Route Path="/community" element={<Community />} />
+          <Route Path="/aboutus" element={<Aboutus />} />
+          <Route Path="/explorer" element={<Explore />} />
+          <Route Path="/faq" element={<Faq />} />
+          <Route Path="/product" element={<Product />} />
+          <Route Path="*" element={<Error />} />
+        </Routes>
+      </Router>
     </>
   );
 }
